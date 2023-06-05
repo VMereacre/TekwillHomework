df5a481a8836ac4b0119303fe2de8bdafceb2ac0
VMereacre <135180179+VMereacre@users.noreply.github.com>
2023-06-05 23:36:48 +0300

    +1

---
+++ lesson_3_homework/homework/home_ex1.py
@@ -0,0 +1,20 @@
+"""1.	Creați 2 variabile x şi y, a căror valoare este citită de la tastatură.
+
+Folosiți funcţia int() pentru a transforma valorile citite în numere întregi.
+
+* Definiţi după aceasta variabila suma care va fi egală cu suma lui x şi y.
+* Definiţi variabila **diff** care va fi egală cu x - y (diferenţa lui x şi y).
+* Definiţi variabila **rest_impart** care va fi egală cu restul împărţirii lui x la y.
+* Definiţi variabila **mult** care va fi egală cu x înmulţit cu y.
+* Definiţi variabila **power3** care va fi egală cu x la puterea 3.
+
+Afișați toate rezultatele
+
+"""
+x = int(input("Dati un nr de tastatura x: "))
+y = int(input("Dati un nr de tastatura y: "))
+diff = x - y
+rest_impart = x % y
+mult = x * y
+power3 = x ** 3
+print(f"Diferenta = {diff}, Restul impartirii = {rest_impart}, Inmultirea = {mult}, x la puterea 3 = {power3}")
---
+++ lesson_3_homework/homework/home_ex2.py
@@ -0,0 +1,10 @@
+"""Utilizatorul va introduce un șir de caractere in consola.
+* Calculați și afișați numarul total de litere în șirului de caractere
+* Calculați și afișați numarul de vocale în șirul de caractere
+* Calculați și afișați numarul de consoane în șirul de caractere
+
+Note: Indiferent daca e majuscula sau minusucula litera
+
+"""
+sir_caractere = input("Introduceti un sir ded caractere :")
+print(len(sir_caractere))
