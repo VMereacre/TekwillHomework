13e025fdd61f003318d49888a38880536798d7d0
VMereacre <135180179+VMereacre@users.noreply.github.com>
2023-06-18 22:50:58 +0300

    home

---
+++ lesson_7_homerwork/homework/home_ex1_a.py
@@ -0,0 +1,27 @@
+'''Exercițiul 1: Calculul factorialului
+Scrieți un program care utilizează o buclă while pentru a calcula factorialul unui număr întreg pozitiv dat n.
+Factorialul unui număr este produsul tuturor numerelor întregi pozitive de la 1 la n.
+'''
+
+nr_init = int(input('Indicati un nr. intreg pozitiv : '))
+
+if nr_init < 0:
+    print(f'{nr_init} este un nr. negativ!')
+else:
+    numar = nr_init
+    factorial = 1
+    while numar >= 1:
+        factorial *= numar
+        numar = numar - 1
+    print(f'Factorial de {nr_init} este {factorial}')
+
+# Varianta cu for
+numar = nr_init
+factorial = 1
+for x in range(1, numar + 1):
+    factorial *= x
+print(factorial)
+
+#Varianta cu math
+import math
+print(f'Factorial de {nr_init} este {math.factorial(numar)}')
